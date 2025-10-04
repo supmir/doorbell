e2c25a2fbaf18f6e08b93ff61cf1d9a568313eb0
Amir <iskandar.amir@gmail.com>
2025-10-04 14:49:53 +0800

    Create install script

---
+++ install.ps1
@@ -0,0 +1,38 @@
+if (-not ([Security.Principal.WindowsPrincipal] `
+        [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+        [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Host "Please run this script as Administrator."
+    exit
+}
+
+if (-not (Get-Command node -ErrorAction SilentlyContinue)){
+    Write-Host "Installing Node.js..."
+    powershell -c "irm https://community.chocolatey.org/install.ps1|iex"
+    choco install nodejs --version="22.20.0"
+} else {
+    Write-Host "Node.js already installed."
+}
+
+
+$apiKey = Read-Host "Enter Discord API Key:"
+$apiKey = Read-Host "Enter Action Channel ID:"
+$apiKey = Read-Host "Enter Log Channel ID:"
+
+[System.Environment]::SetEnvironmentVariable("DOORBELL_TOKEN", $apiKey, "User")
+[System.Environment]::SetEnvironmentVariable("ACTION_CHANNEL_ID", $actionChannelID, "User")
+[System.Environment]::SetEnvironmentVariable("LOG_CHANNEL_ID", $logChannelID, "User")
+
+
+
+$folderPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+$mainScript = Join-Path $folderPath "index.js"
+$startupScript = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\doorbell.cmd"
+
+$nodePath = (Get-Command node).Source
+
+"@echo off
+`"$nodePath`" `"$mainScript`"
+exit" | Out-File -Encoding ASCII $startupScript
+
+
+
--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-https://discord.com/api/oauth2/authorize?client_id=1416656765356871745&permissions=2048&scope=bot%20applications.commands
+Download https://github.com/supmir/doorbell/archive/refs/heads/main.zip
